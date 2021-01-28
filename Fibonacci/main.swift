9e1b3862e6c73984dd6e5d2c0cca57fd4735cb36
Michael Salmon <msalmonse@gmail.com>
2021-01-28 14:08:21 +0100

    Added extra n values Filtered out implementations that can't be used

--- Fibonacci/main.swift
+++ Fibonacci/main.swift
@@ -8,12 +8,12 @@
 import Foundation
 
 let loopCount = 1000
-let nList = [ 1, 2, 3, 6, 9, 14, 21, 32, 48, 72 ]
+let nList = [ 1, 2, 3, 6, 9, 14, 21, 32, 48, 64, 78, 92 ]
 
 // compare implementations
 for n in nList {
-    let impList = Implementation.allCases.shuffled()
-    for i in stride(from: 0, through: impList.count - 1, by: 2) {
+    let impList = Implementation.allCases.shuffled().filter { $0.checkMax(n) }
+    for i in stride(from: 0, through: impList.count - 2, by: 2) {
         _ = test(impList[1], impList[i + 1], n)
     }
 }
