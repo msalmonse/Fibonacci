6b3aa601c3304187e9bda1e268123397415c7af5
Michael Salmon <msalmonse@gmail.com>
2021-01-28 18:35:22 +0100

    Added binet2

--- Fibonacci/TimeImplementation.swift
+++ Fibonacci/TimeImplementation.swift
@@ -18,11 +18,12 @@ func blockRunTime<T>(count: Int = 1, _ block: () -> T) -> Double {
 }
 
 enum Implementation: CaseIterable {
-    case array, binet, iterative, recursive, recursive2, recursive10
+    case array, binet, binet2, iterative, recursive, recursive2, recursive10
     
     func checkMax(_ n: Int) -> Bool {
         switch self {
         case .binet: return n < 72
+        case .binet2: return n < 72
         case .recursive: return n < 40
         default: return true
         }
@@ -32,6 +33,7 @@ enum Implementation: CaseIterable {
         switch self {
         case .array: return "Array"
         case .binet: return "Binet"
+        case .binet2: return "Binet2"
         case .iterative: return "Iterative"
         case .recursive: return "Recursive"
         case .recursive2: return "Recursive2"
@@ -44,6 +46,7 @@ func runImplementation(_ n: Int, imp: Implementation) -> Int {
     switch imp {
     case .array: return fArray(n)
     case .binet: return fBinet(n)
+    case .binet2: return fBinet2(n)
     case .iterative: return fIterative(n)
     case .recursive: return fRecursive(n)
     case .recursive2: return fRecursive2(n)
@@ -55,6 +58,7 @@ func timeImplementation(_ n: Int, count: Int, imp: Implementation) -> Double {
     switch imp {
     case .array: return blockRunTime(count: count) {fArray(n)}
     case .binet: return blockRunTime(count: count) {fBinet(n)}
+    case .binet2: return blockRunTime(count: count) {fBinet2(n)}
     case .iterative: return blockRunTime(count: count) {fIterative(n)}
     case .recursive: return blockRunTime(count: count) {fRecursive(n)}
     case .recursive2: return blockRunTime(count: count) {fRecursive2(n)}
