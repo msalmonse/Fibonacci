1c0af1b77ae727ef25c6d8837a4626d96ce1989b
Michael Salmon <msalmonse@gmail.com>
2021-01-28 13:51:20 +0100

    Added comparison of implementations Used Implementation method checkMax() to see if we should calculate fib(n)

--- Fibonacci/main.swift
+++ Fibonacci/main.swift
@@ -8,12 +8,19 @@
 import Foundation
 
 let loopCount = 1000
-let shiftMax = 6
+let nList = [ 1, 2, 3, 6, 9, 14, 21, 32, 48, 72 ]
+
+// compare implementations
+for n in nList {
+    let impList = Implementation.allCases.shuffled()
+    for i in stride(from: 0, through: impList.count - 1, by: 2) {
+        _ = test(impList[1], impList[i + 1], n)
+    }
+}
 
 for imp in Implementation.allCases {
-    for i in 0...shiftMax {
-        let n = 1 << i
-        if imp != .recursive || n <= 32 {
+    for n in nList {
+        if imp.checkMax(n) {
             let runTime = String(format: "%.1f",
                 timeImplementation(n, count: loopCount, imp: imp)/Double(loopCount)
             )
