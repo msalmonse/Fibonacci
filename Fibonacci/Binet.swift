c58481bc6f8525d6fd64bb5436516ed4a8e21b8b
Michael Salmon <msalmonse@gmail.com>
2021-02-07 13:52:59 +0100

    Changed binet() to use Decimal exp()

--- Fibonacci/Binet.swift
+++ Fibonacci/Binet.swift
@@ -8,15 +8,15 @@
 import Foundation
 
 private let root5 = sqrt(5.0)
-private let log2root5 = log2(root5)
+private let logroot5 = Decimal(log(root5))
 private let phi = (1.0 + root5)/2.0
-private let log2phi = log2(phi)
+private let logphi = Decimal(log(phi))
 
 func fBinet(_ n: Int) -> Decimal {
     guard n > 0 else { return 0 }
 
     // pow(phi, n) == exp2(log2(phi)) * n
-    return Decimal(round(exp2(log2phi * Double(n) - log2root5)))
+    return round(exp(logphi * Decimal(n) - logroot5))
 }
 
 func fBinet2(_ n: Int) -> Decimal {
