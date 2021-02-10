add3e78aebab28336370c942c9df9a3f6b09dfb2
Michael Salmon <msalmonse@gmail.com>
2021-02-10 08:36:02 +0100

    Changed to do calculation in doubles

--- Fibonacci/Binet.swift
+++ Fibonacci/Binet.swift
@@ -8,20 +8,21 @@
 import Foundation
 
 private let root5 = sqrt(5.0)
-private let logroot5 = Decimal(log(root5))
+private let invRoot5 = 1.0/root5
+private let logroot5 = log(root5)
 private let phi = (1.0 + root5)/2.0
-private let logphi = Decimal(log(phi))
+private let logphi = log(phi)
 
 func fBinet(_ n: Int) -> Decimal {
     guard n > 0 else { return 0 }
 
-    // pow(phi, n) == exp2(log2(phi)) * n
-    return round(exp(logphi * Decimal(n) - logroot5))
+    // pow(phi, n) == exp(log(phi)) * n
+    return round(Decimal(exp(logphi * Double(n) - logroot5)))
 }
 
 func fBinet2(_ n: Int) -> Decimal {
     guard n > 0 else { return 0 }
 
     // pow(phi, n) == exp2(log2(phi)) * n
-    return round(pow(Decimal(phi), n)/Decimal(root5))
+    return round(Decimal(pow(phi, Double(n)) * invRoot5))
 }
