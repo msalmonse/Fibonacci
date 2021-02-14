de38747772d1a18f5f7e3357e63b08392c9ada01
Michael Salmon <msalmonse@gmail.com>
2021-02-14 10:32:21 +0100

    Added hybrid implementation

--- Fibonacci/TimeImplementation.swift
+++ Fibonacci/TimeImplementation.swift
@@ -18,7 +18,7 @@ func blockRunTime<T>(count: Int = 1, _ block: () -> T) -> Double {
 }
 
 enum Implementation: CaseIterable {
-    case array, binet, binet2, iterative, recursive, recursive2, recursive10
+    case array, binet, binet2, iterative, recursive, recursive2, recursive10, hybrid
 
     func checkMax(_ n: Int) -> Bool {
         switch self {
@@ -36,9 +36,10 @@ enum Implementation: CaseIterable {
         switch self {
         case .binet: return 1000
         case .binet2: return 1000
+        case .hybrid: return 50
         case .iterative: return 1000
         case .recursive: return 10000
-        case .recursive2: return 100
+        case .recursive2: return 50
         case .recursive10: return 50
         default: return 1
         }
@@ -49,6 +50,7 @@ enum Implementation: CaseIterable {
         case .array: return "Array"
         case .binet: return "Binet"
         case .binet2: return "Binet2"
+        case .hybrid: return "Hybrid"
         case .iterative: return "Iterative"
         case .recursive: return "Recursive"
         case .recursive2: return "Recursive2"
@@ -62,6 +64,7 @@ func runImplementation(_ n: Int, imp: Implementation) -> Decimal {
     case .array: return fArray(n)
     case .binet: return fBinet(n)
     case .binet2: return fBinet2(n)
+    case .hybrid: return fHybrid(n)
     case .iterative: return fIterative(n)
     case .recursive: return fRecursive(n)
     case .recursive2: return fRecursive2(n)
@@ -74,6 +77,7 @@ func timeImplementation(_ n: Int, count: Int, imp: Implementation) -> Double {
     case .array: return blockRunTime(count: count) {fArray(n)}
     case .binet: return blockRunTime(count: count) {fBinet(n)}
     case .binet2: return blockRunTime(count: count) {fBinet2(n)}
+    case .hybrid: return blockRunTime(count: count) {fHybrid(n)}
     case .iterative: return blockRunTime(count: count) {fIterative(n)}
     case .recursive: return blockRunTime(count: count) {fRecursive(n)}
     case .recursive2: return blockRunTime(count: count) {fRecursive2(n)}
